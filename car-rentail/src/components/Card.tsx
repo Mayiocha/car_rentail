822cfca7e27e442bac9051dc20157b1dbb2d1636
Mayiocha <josemadrid09z@gmail.com>
2026-04-27 11:52:05 -0600

    cc2

---
+++ car-rentail/src/components/Card.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Card(){
+    const aumentar = () =>{
+        console.log("Hola")
+    }
+    return(
+        <>
+            <h1>Card</h1>
+            <button>Hola</button>
+        </>
+    )
+}
