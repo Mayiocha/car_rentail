38985df9aea8b1def18d5ddc4b51eebc0c62cfae
Mayiocha <josemadrid09z@gmail.com>
2026-04-29 11:58:56 -0600

    cc4

--- car-rentail/src/App.tsx
+++ car-rentail/src/App.tsx
@@ -1,26 +1,11 @@
-import { useState } from 'react'
-import Card from './components/Card'
+import Dashboard from './views/Dashboard'
+import './App.css'
 
 function App() {
-  const [elementos, setElementos] = useState([]) as any
-  const [nombre, setNombre] = useState("")
 
-  const agregar= ()=>{
-    setElementos([...elementos, nombre])
-    
-  }
   return (
     <>
-      <h1>{nombre}</h1>
-      <input type="text" value={nombre} onChange={  (e)=>{ setNombre(e.target.value) }  } />
-      <button onClick={agregar}>Agregar</button>
-      {
-        elementos.map( ()=>{
-          return <Card/>
-        })
-      }
-      
-      
+      <Dashboard />
     </>
   )
 }
